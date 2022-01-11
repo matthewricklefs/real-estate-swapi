30d496255959b6709539c54b1f2b2fa6d395badf
matthewricklefs <ricklefsmatthew1003@gmail.com>
2022-01-11 17:26:44 -0500

    debugs react-icons import, updates global button styles, adds Section2 to the Home.jsx page.

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Section1 from "./Sections/Section1";
+import Section2 from "./Sections/Section2";
 
 const Home = () => {
 	return (
 		<div>
 			<Section1 />
+			<Section2 />
 		</div>
 	);
 };
