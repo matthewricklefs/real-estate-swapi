5034eccc4528acae7d7ccf84e0859a5a2a63f394
matthewricklefs <ricklefsmatthew1003@gmail.com>
2022-01-11 18:29:46 -0500

    adds SwiperSlide, Carousel, Card inside of Section3.jsx, creates Section3 and styles, adds Section3 to Home.jsx page

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Section1 from "./Sections/Section1";
 import Section2 from "./Sections/Section2";
+import Section3 from "./Sections/Section3";
 
 const Home = () => {
 	return (
 		<div>
 			<Section1 />
 			<Section2 />
+			<Section3 />
 		</div>
 	);
 };
--- src/components/Pages/Home/Sections/Section3.jsx
+++ src/components/Pages/Home/Sections/Section3.jsx
@@ -1,7 +1,40 @@
 import styles from "./Section3.module.scss";
+import { ArrowButton } from "../../../Buttons/Buttons";
+import Carousel from "../../../Carousel/Carousel";
+
+import { SwiperSlide } from "swiper/react";
+import Card from "../../../Card/Card";
 
 const Section3 = () => {
-	return <div></div>;
+	return (
+		<section className={styles.section_3}>
+			<div className={styles.section_3_title}>
+				<h1>Best Houses</h1>
+				<ArrowButton text="See more.." />
+			</div>
+
+			{/* Cards & Carousel */}
+			<div className={styles.cards}>
+				<Carousel>
+					<SwiperSlide>
+						<Card />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Card />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Card />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Card />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Card />
+					</SwiperSlide>
+				</Carousel>
+			</div>
+		</section>
+	);
 };
 
 export default Section3;
