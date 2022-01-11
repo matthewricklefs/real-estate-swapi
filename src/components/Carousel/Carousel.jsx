f9e442a0ebb1c628ca2d618710f5884755217d10
matthewricklefs <ricklefsmatthew1003@gmail.com>
2022-01-11 18:29:03 -0500

    creates Carousel component using swiper dependency, creates Carousel styles, Carousel can now be dragged and swiped to browse listings

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,12 +2,15 @@ import { Swiper } from "swiper/react";
 import { Pagination } from "swiper";
 import "./Carousel.scss";
 
+import "swiper/scss";
+import "swiper/scss/pagination";
+
 const Carousel = ({ children, slides }) => {
 	return (
 		<Swiper
 			modules={[Pagination]}
 			spaceBetween={50}
-			sliderPerView={slides}
+			slidesPerView={slides}
 			pagination={{ clickable: true }}
 		>
 			{children}
@@ -15,6 +18,10 @@ const Carousel = ({ children, slides }) => {
 			<div style={{ marginTop: "70px" }}></div>
 		</Swiper>
 	);
+};
+
+Carousel.defaultProps = {
+	slides: "auto",
 };
 
 export default Carousel;
