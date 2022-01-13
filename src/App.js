8c1856f63016c41ba9d4c00e0a925e58ea72b238
Matthew Ricklefs <matthew@Matthews-MacBook-Pro.local>
2022-01-13 15:05:37 -0500

    debugs yarn.lock, installs apollo/client and graphql, wraps app inside of ApolloClient, creates graphql to fetch all of the available listings in our strapi content collection

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "../src/components/Pages/Home/Home";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</header>
-		</div>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </header>
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- src/components/Pages/Home/Sections/Section3.jsx
+++ src/components/Pages/Home/Sections/Section3.jsx
@@ -4,37 +4,90 @@ import Carousel from "../../../Carousel/Carousel";
 
 import { SwiperSlide } from "swiper/react";
 import Card from "../../../Card/Card";
+import { useQuery, gql } from "@apollo/client";
+
+const CARD_DATA = gql`
+  query {
+    houses(
+      filters: { categories: { Category: { contains: "Buy" } } }
+      pagination: { limit: 6 }
+    ) {
+      data {
+        id
+        attributes {
+          location {
+            data {
+              attributes {
+                City
+              }
+            }
+          }
+          Preview_Image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+          categories {
+            data {
+              attributes {
+                Category
+              }
+            }
+          }
+
+          Street
+          Rooms
+          Bedrooms
+          Bathrooms
+          Price
+          Rent
+          Short_Address
+          Neighborhood
+        }
+      }
+    }
+  }
+`;
 
 const Section3 = () => {
-	return (
-		<section className={styles.section_3}>
-			<div className={styles.section_3_title}>
-				<h1>Best Houses</h1>
-				<ArrowButton text="See more.." />
-			</div>
+  const { loading, data, error } = useQuery(CARD_DATA);
 
-			{/* Cards & Carousel */}
-			<div className={styles.cards}>
-				<Carousel>
-					<SwiperSlide>
-						<Card />
-					</SwiperSlide>
-					<SwiperSlide>
-						<Card />
-					</SwiperSlide>
-					<SwiperSlide>
-						<Card />
-					</SwiperSlide>
-					<SwiperSlide>
-						<Card />
-					</SwiperSlide>
-					<SwiperSlide>
-						<Card />
-					</SwiperSlide>
-				</Carousel>
-			</div>
-		</section>
-	);
+  return (
+    <section className={styles.section_3}>
+      <div className={styles.section_3_title}>
+        <h1>Best Houses</h1>
+        <ArrowButton text="See more.." />
+      </div>
+
+      {/* Cards & Carousel */}
+      <div className={styles.cards}>
+        <Carousel>
+          {loading || error ? (
+            <>
+              <SwiperSlide>
+                <Card />
+              </SwiperSlide>
+            </>
+          ) : (
+            <>
+              {data.houses.data.map((house, index) => (
+                <SwiperSlide key={index}>
+                  <Card
+                    info={{
+                      category: "Buy",
+                      imageSource: `http://localhost:1337/${house.attributes.Preview_Image}`,
+                    }}
+                  />
+                </SwiperSlide>
+              ))}
+            </>
+          )}
+        </Carousel>
+      </div>
+    </section>
+  );
 };
 
 export default Section3;
